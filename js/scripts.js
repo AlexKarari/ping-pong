eb5a8e62fcd94f54ef0d1d954f481e4c144c0e4f
Alexander Karari <karari.alexander@gmail.com>
2018-02-23 17:57:12 +0300

    Add jquerry function to display the results.

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $("#enter form").submit(function(event) {
+        event.preventDefault();
+      var display = parseInt($("input#ppnumber").val());
+      var result = answer(display);
+      $(".Push").text(result);
+
+    });
+  });
+
+// business logic
+var answer=function(display){
+  for(i=1; i<=display; i++){
+ // if the number is divisible by 15, write "Ping Pong"
+  if ( i % 15 === 0) {
+    console.log("pingpong");
+    $(".Push").append("<li>"+"pingpong"+"</>");
+    //return "PingPong";
+   }
+  // if the number is divisible by 3, write "Ping"
+  else if ( i % 3 === 0) {
+    $(".Push").append("<li>"+"ping"+"</>");
+    //return "Ping";
+  }
+    // otherwise, write "po"
+  else if ( i % 5 === 0) {
+    $(".Push").append("<li>"+"pong"+"</>");
+      //return "Pong";
+     }
+  else {
+      console.log("qwert");
+      $(".Push").append("<li>"+i+"</>");
+    //return(i);
+  }
+}
+}
