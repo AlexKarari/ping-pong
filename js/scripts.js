66df8e39eb95d025036a4b4204422a014ed9897a
Alexander Karari <karari.alexander@gmail.com>
2018-02-27 16:52:04 +0300

    Final changes
    All changes touching on project's objectives have been achieved.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 // User Interface
 $(document).ready(function() {
-  $("#open-menu").click(function(){
-  $("aside").animate({
-    "left":0
-  },2000);
+  $("#open-menu").click(function() {
+    $("aside").animate({
+      "left": 0
+    }, 2000);
   });
   $("#close-menu").click(function() {
     $("aside").animate({
